d1310399bc23c73a78caaad4aaa25211c06df436
Ken Gibson <klgibsonjr@kens-mbp.lan>
2022-12-27 18:25:23 -0800

    added routes folder, added middleware to server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT),
   () => {
