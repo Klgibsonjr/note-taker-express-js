314897af8b884d63484448b57ddf1a6839411cea
klgibsonjr <klgibsonjr@gmail.com>
2023-01-05 15:37:23 -0800

    created html routes

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 require('./routes/routes');
 
