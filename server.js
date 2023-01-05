dd480da18ab96f27053a53f7d2932e61c4124114
klgibsonjr <klgibsonjr@gmail.com>
2023-01-05 14:41:29 -0800

    organized file structure

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  res.send('Home');
+});
+
+app.get('/notes', (req, res) => {
+  res.send('Notes');
+});
+
 app.listen(PORT),
   () => {
     console.log('App listening on PORT: ' + PORT);
