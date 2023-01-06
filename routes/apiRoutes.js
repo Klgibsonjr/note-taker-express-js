206ba0820951229384f220b92615f304efad9ae5
klgibsonjr <klgibsonjr@gmail.com>
2023-01-06 00:45:46 -0800

    made no progress, not a fan of express.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,26 @@
-app.get('/api/notes', (req, res) => {});
+const express = require('express');
+const router = express.Router();
+const uuid = require('uuid');
+const fs = require('fs');
+
+fs.readFile('db/db.json', 'utf8', (err, data) => {
+  if (err) throw err;
+  let notes = JSON.parse(data);
+  router.get('/api/notes', (req, res) => {
+    res.json();
+  });
+
+  router.post('/api/notes', (req, res) => {
+    req.body.id = uuid();
+    let newNote = req.body;
+    notes.push(newNote);
+    updateDB();
+  });
+
+  const updateDB = () => {
+    fs.writeFile(noteData, JSON.stringify(noteData, '\t'), (err) => {
+      if (err) throw err;
+      return;
+    });
+  };
+});
--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 const htmlRoutes = require('./routes/htmlRoutes');
-// const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', htmlRoutes);
-// app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
